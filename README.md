c5d9ccef6a72b2329a0be65a43c95ed3df732de0
Zoszen <walkerwjoseph@gmail.com>
2022-06-30 09:34:22 +0100

    A complete Redo
    I went back and restarted all the tutorial, trying to understand every bit.

--- README.md
+++
@@ -1,2 +0,0 @@
-# RogueLike-2022
-Hi, this is my version of the 2022 Roguelikedev does the Roguelike tutorial.
--- main.py
+++ main.py
@@ -1,47 +1,54 @@
 #!/usr/bin/env python3
-import tcod
-
-from engine import Engine
-from entity import Entity
-from game_map import GameMap
+from actions import EscapeAction, MovementAction
 from input_handlers import EventHandler
-
+import tcod
+WIDTH, HEIGHT = 80, 60
 
 def main() -> None:
-    screen_width = 80
-    screen_height = 50
 
-    map_width = 80
-    map_height = 45
+    screen_width=80
+    screen_height=50
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
 
-    tileset = tcod.tileset.load_tilesheet(
-        "Tileset.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+
+
+    tileset= tcod.tileset.load_tilesheet(
+    "Tileset.png", 32, 8,  tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
-
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "T", (255, 255, 0))
-    entities = {npc, player}
-
-    game_map = GameMap(map_width, map_height)
-
-    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
-
-    with tcod.context.new_terminal(
-        screen_width,
-        screen_height,
-        tileset=tileset,
-        title="Caverns",
-        vsync=True,
+    #creating main console:
+    console = tcod.Console(screen_width,screen_height, order="F")
+    # Create a window based on this console and tileset.
+    with tcod.context.new(
+       columns=console.width, rows=console.height, tileset=tileset, title="Woodlands", vsync=True,
     ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            engine.render(console=root_console, context=context)
+            console.clear()
+            console.print(x=player_x, y=player_y, string="@")
+            context.present(console)#show the console
 
-            events = tcod.event.wait()
+            for event in tcod.event.wait():
+                context.convert_event(event)# Sets tile coordinates for mouse events.
+                print (event)
+                #checking if an action is happening?
+                action = event_handler.dispatch(event)
+                #if there is no actions, the skip the rest I guess?
+                if action is None:
+                    continue
+              
+                if isinstance(action, MovementAction):
+                    # if there is an instance of a movement action 
+                    #changing the player X,Y if there is a movemnet action
+                    player_x += action.dx
+                    player_y += action.dy
 
-            engine.handle_events(events)
+                elif isinstance(action, EscapeAction):
+                    #obvs another escape action..
+                    raise SystemExit()
+
+
 
 
 if __name__ == "__main__":
